7c3461621e16ee16ef88c8e06c095bfe09c05d59
hooss-only <hoossonly@gmail.com>
2025-08-30 17:30:19 +0900

    Add exit enum

---
+++ src/exit.h
@@ -0,0 +1,5 @@
+enum exit_code_t {
+	EXIT_OK = 0,
+	EXIT_USAGE = 1,
+	EXIT_FILE_ERR = 2,
+};
--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "usage.h"
+#include "exit.h"
+#include "file.h"
 
 void handle_args(int argc, char* argv[]);
 void add_todo(int argc, char* argv[]);
@@ -8,15 +12,20 @@ void add_todo(int argc, char* argv[]);
 int main(int argc, char* argv[]) {
 	handle_args(argc, argv);
 
-	return 0;
+	return EXIT_OK;
 }
 
 void handle_args(int argc, char* argv[]) {
-	if (argc < 2) usage(1);
-
-	if (!strcmp(argv[1], "add")) add_todo(argc, argv);
+	if (argc < 2) usage(EXIT_USAGE);
+	
+	char* cmd = argv[1];
+	if (!strcmp(cmd, "add")) 				add_todo(argc, argv);
+	else if (!strcmp(cmd, "help"))	usage(EXIT_OK);
 }
 
 void add_todo(int argc, char* argv[]) {
-	if (argc < 3) usage(1);
+	if (argc < 3) usage(EXIT_USAGE);
+	if (find_todo() == -1) exit(EXIT_FILE_ERR);
+	
+	printf("hi\n");
 }
